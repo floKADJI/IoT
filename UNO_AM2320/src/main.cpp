e5a38e4a7e4e76a5354033be711234c34bbc9384
Florian <kadjiflorian@gmail.com>
2021-09-01 21:46:13 +0100

    testing AM2320 on Arduino UNO board

--- UNO_AM2320/src/main.cpp
+++ UNO_AM2320/src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
+#include "Adafruit_Sensor.h"
+#include "Adafruit_AM2320.h"
+
+Adafruit_AM2320 am2320 = Adafruit_AM2320();
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(10); // hang out until serial port opens
+  }
+
+  Serial.println("Adafruit AM2320 Basic Test");
+  am2320.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("Temp: "); Serial.println(am2320.readTemperature());
+  Serial.print("Hum: "); Serial.println(am2320.readHumidity());
+
+  delay(2000);
 }
