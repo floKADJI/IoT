1adbe846e007812cb92dc2995eaef9b4e170b441
Florian <kadjiflorian@gmail.com>
2021-09-13 20:45:30 +0100

    using Sensors & display on ESP32 board

---
+++ ESP32_Sensors_Display/src/main.cpp
@@ -0,0 +1,130 @@
+#include <Arduino.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#include "Adafruit_Sensor.h"
+#include "Adafruit_AM2320.h"
+
+#include "DHT.h"
+
+
+
+
+  // For the breakout board, you can use any 2 or 3 pins.
+  // These pins will also work for the 1.8" TFT shield.
+  #define TFT_CS         5
+  #define TFT_RST        4 // Or set to -1 and connect to Arduino RESET pin
+  #define TFT_DC         2
+
+#define DHTPIN 27     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11   // DHT 11
+
+
+// For 1.44" and 1.8" TFT with ST7735 use:
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
+
+// Initialize AM2320 sensor.
+Adafruit_AM2320 am2320 = Adafruit_AM2320();
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+
+float p = 3.1415926;
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(" seconds.");
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Serial.print(F("Hello! ST77xx TFT Test"));
+
+  // use this initializer if using a 1.44" TFT:
+  tft.initR(INITR_144GREENTAB); // Init ST7735R chip, green tab
+
+  Serial.println(F("Initialized"));
+
+  // tft print function!
+  tftPrintTest();
+  delay(4000);
+
+
+  Serial.println("Adafruit AM2320 Basic Test");
+  am2320.begin();
+
+  
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  delay(2000);
+
+  Serial.print("Temp: "); Serial.println(am2320.readTemperature());
+  Serial.print("Hum: "); Serial.println(am2320.readHumidity());
+
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+}
