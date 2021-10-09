f25b875472bb8ddc7e010d8fba9a391eed694d11
Florian <kadjiflorian@gmail.com>
2021-10-09 10:57:38 +0100

    test WiFi.onEvent function for WiFi_Events callback

--- ESP32_WIFI/src/main.cpp
+++ ESP32_WIFI/src/main.cpp
@@ -6,68 +6,35 @@ const char *ssid = "IoT_Network";         // Home network ssid = "Yoomee-A1CE"
 const char *password = "password";        // Home network pwd = "wifi_007KJ"
 // There are global variable used to establish connection to AP
 
-// Variables of time to manage the delay between differents operations
-unsigned long now;
-unsigned long last_conn = 0;
-const int INTERVAL_CONNECTION = 60000; // 1 min
 
-
-/**
- * Function to setup the WiFi connection to AP with network credentials given
- * Params: Null
- * Return: Null
- */
-void wifi_setup(){
-  Serial.println();
-  // Initialisation of Connection to WiFi
-  WiFi.begin(ssid, password);
-  // Serial.printf("Connection status: %d \n", WiFi.status());
-  // TODO: Find best solution
-  delay(1000); // Time for connection
-
-  if (WiFi.status() == WL_CONNECTED) {
-      // Print local IP address
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info)
+{
     Serial.println("WiFi connected");
-    Serial.print("IP address: ");
+    Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-    Serial.printf("Connection status: %d \n", WiFi.status());
-  }
-
-  
 }
 
+void setup()
+{
+    Serial.begin(115200);
 
-void setup() {
-  
-  // Configure the LED_BUILTIN to ligth during the setup step to help debugging
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);  // Just for checking, starting setup
+    // delete old config
+    WiFi.disconnect(true);
 
-  Serial.begin(115200);   // Serial Monitor Baud rate setup
+    delay(1000);
 
-  // Call wifi_setup() fonction for WIFI configuration
-  wifi_setup();
+    // Examples of different ways to register wifi events
+    //WiFi.onEvent(WiFiGotIP);
+    WiFi.onEvent(WiFiGotIP, WiFiEvent_t::SYSTEM_EVENT_STA_GOT_IP);
+    
+    WiFi.begin(ssid, password);
 
-  digitalWrite(LED_BUILTIN, LOW);   // Just for checking that we have done with Setup
+    Serial.println();
+    Serial.println();
+    Serial.println("Wait for WiFi... ");
 }
 
-void loop() {
-  
-  // Starting the timer;
-  now = millis();
-  
-  // Process to chek if connection WiFi connection is loss, then try to reconnect
-  if (now - last_conn >= INTERVAL_CONNECTION) {
-    last_conn = now;
-
-    // Check connection
-    if (WiFi.status() != WL_CONNECTED) {
-      wifi_setup();
-      delay(1000);
-      Serial.println(".");
-    //  Serial.printf("Connection status: %d \n", WiFi.status());
-    }
-
-  }
-
+void loop()
+{
+    delay(1000);
 }
