cff7b6e0863260677cb776a5feea1ab88ccf2412
Florian <kadjiflorian@gmail.com>
2021-10-19 20:26:15 +0100

    testing Lib PCF8583.h

---
+++ ESP32_PCF8583/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include "PCF8583.h"
+
+PCF8583 rtc(0xA0);
+
+void assertEquals(const char * test, byte expected, byte real) {
+    if (expected == real) {
+        Serial.println("OK");
+    } else {
+        Serial.print("KO: ");
+        Serial.println(test);
+    }
+}
+
+void setup() {
+
+    Serial.begin(115200);
+
+    rtc.setTime(0, 10, 20);
+    assertEquals("getSeconds", 0, rtc.getSecond());
+    assertEquals("getMinutes", 10, rtc.getMinute());
+    assertEquals("getHours", 20, rtc.getHour());
+
+    delay(2000);
+    assertEquals("getSeconds +2", 2, rtc.getSecond());
+
+}
+
+void loop() {}
