f7587dd6c5f487fd31290e6ed1e79714858151fe
Florian <kadjiflorian@gmail.com>
2022-04-18 20:23:53 +0100

    using Lib SPI.h & "FS.h" on Boards for branch SPIFFS

---
+++ ESP32_SPIFFS/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+
+// The SPIFFS (FLASH filing system) is used to hold configuration file
+
+#include "FS.h"
+
+// EEPROM added to save password type on the keypad from touch screen
+#include <EEPROM.h>
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
